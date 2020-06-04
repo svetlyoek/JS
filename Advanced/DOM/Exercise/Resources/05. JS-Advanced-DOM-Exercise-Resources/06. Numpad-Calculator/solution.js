90dc50f42b72eff378b3b450b5b28ee35c1de54d
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-06-04 12:11:25 +0300

    Add new

--- Advanced/DOM/Exercise/Resources/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
+++ Advanced/DOM/Exercise/Resources/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
@@ -4,56 +4,43 @@ function solve() {
     const expOutput = document.getElementById('expressionOutput');
     const resultOutput = document.getElementById('resultOutput');
     let result = 0;
-    let leftOperand = '';
-    let rightOperand = '';
-    let operator = '';
-    let counter = 0;
-
+    let leftOperand = 0;
+    let rightOperand = 0;
+    let splittedResult = '';
 
     document.querySelectorAll('.keys > button').forEach(key => {
         key.addEventListener('click', function (e) {
+            if (key.value !== '=') {
+                expOutput.textContent += key.value;
+            } else if (key.value === '=') {
 
-            if (key.value === '=') {
+                splittedResult = expOutput.textContent.split(/[^\w.]/);
+                leftOperand = Number(splittedResult[0]);
+                rightOperand = Number(splittedResult[1]);
+
+                if (leftOperand === 0 || rightOperand === 0) {
+                    resultOutput.textContent = 'NaN';
+                    return;
+                }
+
                 if (expOutput.textContent.includes('/')) {
-                    result = Number(leftOperand) / Number(rightOperand);
+
+                    result = leftOperand / rightOperand;
+
                 } else if (expOutput.textContent.includes('*')) {
-                    result = Number(leftOperand) * Number(rightOperand);
+
+                    result = leftOperand * rightOperand;
+
                 } else if (expOutput.textContent.includes('-')) {
-                    result = Number(leftOperand) - Number(rightOperand);
+
+                    result = leftOperand - rightOperand;
+
                 } else if (expOutput.textContent.includes('+')) {
-                    result = Number(leftOperand) + Number(rightOperand);
+
+                    result = leftOperand + rightOperand;
                 }
 
                 resultOutput.textContent = result;
-
-                leftOperand = '';
-                rightOperand = '';
-                operator = '';
-            } else if (key.value !== '=' && key.value !== '/' && key.value !== '*' && key.value !== '-'
-                && key.value !== '+') {
-
-                expOutput.textContent += key.value;
-
-                if (!expOutput.textContent.includes('/') && !expOutput.textContent.includes('*') &&
-                    !expOutput.textContent.includes('+') && !expOutput.textContent.includes('-')) {
-                    if (expOutput.textContent.length > 0) {
-                        leftOperand += key.value;
-                    } else if (expOutput.textContent.length == 0)
-                        leftOperand = key.value;
-                } else if (expOutput.textContent.includes('/') && expOutput.textContent.includes('*') &&
-                    expOutput.textContent.includes('+') && expOutput.textContent.includes('-')) {
-                    if (expOutput.textContent.length > 0) {
-                        rightOperand += key.value;
-                    } else if (expOutput.textContent.length == 0) {
-                        rightOperand = key.value;
-                    }
-
-                }
-
-            } else if (key.value === '=' || key.value === '/' || key.value === '*' || key.value === '-'
-                || key.value === '+') {
-                expOutput.textContent += key.value;
-                operator = key.value;
             }
 
         })
@@ -63,8 +50,4 @@ function solve() {
         document.getElementById('expressionOutput').textContent = '';
         document.getElementById('resultOutput').textContent = '';
     });
-
-
-
-
 }
--- Advanced/DOM/Exercise/Resources/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
+++ Advanced/DOM/Exercise/Resources/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
@@ -1,4 +1,32 @@
 function solve() {
+    const menuTo = document.getElementById('selectMenuTo');
+    const button = document.querySelector('button');
+    const resultField = document.getElementById('result');
 
-    //TODO...
+    const inputNumber = document.getElementById('input');
+
+    const binaryOption = document.createElement('option');
+    binaryOption.value = 'binary';
+    binaryOption.textContent = 'Binary';
+    menuTo.appendChild(binaryOption);
+
+    const hexaOption = document.createElement('option');
+    hexaOption.value = 'hexadecimal';
+    hexaOption.textContent = 'Hexadecimal';
+    menuTo.appendChild(hexaOption);
+
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        switch (menuTo.value) {
+            case 'binary': resultField.value = Number(inputNumber.value).toString(2);
+                break;
+            case 'hexadecimal': resultField.value = Number(inputNumber.value).toString(16).toUpperCase();
+                break;
+            default:
+                break;
+        }
+
+    });
+
 }
