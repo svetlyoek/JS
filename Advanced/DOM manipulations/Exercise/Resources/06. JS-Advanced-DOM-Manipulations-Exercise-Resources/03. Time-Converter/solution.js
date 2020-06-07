663596731852795a437ffddceef9bfa7c2013ebc
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-06-07 20:01:32 +0300

    Add new

--- Advanced/DOM manipulations/Exercise/Resources/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/03. Time-Converter/solution.js
+++ Advanced/DOM manipulations/Exercise/Resources/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/03. Time-Converter/solution.js
@@ -1,4 +1,39 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    const buttons = Array.from(document.querySelectorAll('input[type="button"]'));
+    const inputs = Array.from(document.querySelectorAll('input[type="text"]'));
+
+    for (let i = 0; i < buttons.length; i++) {
+
+        let currentButton = buttons[i];
+
+        currentButton.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if (currentButton == e.target && i == 0) {
+                let days = Number(inputs[0].value);
+                inputs[1].value = (days * 24).toString();
+                inputs[2].value = (days * 1440).toString();
+                inputs[3].value = (days * 86400).toString();
+            }
+            if (currentButton == e.target && i == 1) {
+                let hours = Number(inputs[1].value);
+                inputs[0].value = (hours / 24).toString();
+                inputs[2].value = (Number(inputs[0].value * 1440).toString()).toString();
+                inputs[3].value = (Number(inputs[0].value * 86400).toString()).toString();
+            }
+            if (currentButton == e.target && i == 2) {
+                let minutes = Number(inputs[2].value);
+                inputs[0].value = (minutes / 1440).toString();
+                inputs[1].value = (Number(minutes / 1440 * 24).toString()).toString();
+                inputs[3].value = (Number(minutes / 1440 * 86400).toString()).toString();
+            }
+            if (currentButton == e.target && i == 3) {
+                let seconds = Number(inputs[3].value);
+                inputs[0].value = (seconds / 86400).toString();
+                inputs[1].value = (Number(seconds / 86400 * 24).toString()).toString();
+                inputs[2].value = (Number(seconds / 86400 * 1440).toString()).toString();
+            }
+        })
+    }
 }
--- Advanced/DOM manipulations/Exercise/Resources/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
+++ Advanced/DOM manipulations/Exercise/Resources/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.querySelectorAll('button'));
+    const radios = Array.from(document.querySelectorAll('input[value="unlock"]'));
+    const hiddenFields = Array.from(document.querySelectorAll('#user1HiddenFields, #user2HiddenFields, #user3HiddenFields'));
+    console.log(hiddenFields);
+    for (let i = 0; i < buttons.length; i++) {
+
+        let currentBtn = buttons[i];
+
+        currentBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if (!radios[i].checked) {
+                return;
+            } else if (radios[i].checked && currentBtn.textContent === 'Hide it') {
+                currentBtn.textContent = 'Show more';
+                hiddenFields[i].style.display = 'none';
+            } else if (radios[i].checked && currentBtn.textContent === 'Show more') {
+
+                currentBtn.textContent = 'Hide it';
+                hiddenFields[i].style.display = 'block';
+            }
+        })
+    }
 }
