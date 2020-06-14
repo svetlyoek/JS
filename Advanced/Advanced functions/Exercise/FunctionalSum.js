2408b08747be633b09ccf6e39b375af84721c6c5
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-06-14 13:51:53 +0300

    Add new

---
+++ Advanced/Advanced functions/Exercise/FunctionalSum.js
@@ -0,0 +1,14 @@
+(function add() {
+
+    let sum = 0;
+
+    return function add(number) {
+        sum += number;
+
+        add.toString = function () {
+            return sum;
+        }
+
+        return add;
+    }
+})()
