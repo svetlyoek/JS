ed88d67a5241c5a35b16b6138f8ee794788a7d18
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-06-22 12:04:39 +0300

    Add new

--- Advanced/Prototypes and inheritance/Exercise/ArrayExtension.js
+++ Advanced/Prototypes and inheritance/Exercise/ArrayExtension.js
@@ -1,46 +1,21 @@
-(function solve() {
-
+(function arrayExtension() {
     Array.prototype.last = function () {
-
         return this[this.length - 1];
     };
 
-    Array.prototype.skip = n = function () {
-
-        this._validation(n);
-
-        return this.slice(n, this.length);
+    Array.prototype.skip = function (n) {
+        return this.slice(n);
     };
 
-    Array.prototype.take = n = function () {
-
-        this._validation(n);
-
-        return this.splice(0, n);
+    Array.prototype.take = function (n) {
+        return this.slice(0, n);
     };
 
     Array.prototype.sum = function () {
-
-        return this.reduce((acc, curr) => {
-
-            acc += curr;
-
-            return acc;
-        });
+        return this.reduce((a, b) => a + b);
     };
 
     Array.prototype.average = function () {
-
         return this.sum() / this.length;
-    };
-
-    function _validation(index) {
-
-        if (number < 0 || number >= this.length) {
-
-            throw new Error('Index is out of range.')
-        }
     }
-
 }())
-
