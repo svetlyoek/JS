65f76acb18c313fdd41d0e746dce2df9f2428848
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-07-06 13:46:56 +0300

    Add new

--- Applications/Rest services and AJAX/Exercise/Resources/04.MESSENGER/app.js
+++ Applications/Rest services and AJAX/Exercise/Resources/04.MESSENGER/app.js
@@ -1,5 +1,67 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const messages = document.getElementById('messages');
+    const author = document.getElementById('author');
+    const message = document.getElementById('content');
+
+    const url = `http://localhost:3000/messenger`;
+
+    sendBtn.addEventListener('click', function (e) {
+
+        if (author.value !== '' && message.value !== '') {
+
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify({
+                    author: author.value,
+                    content: message.value
+                })
+            })
+                .then((response) => {
+
+                    if (!response.ok) {
+
+                        console.error('Something get wrong!');
+
+                    } else {
+
+                        return response.json();
+                    }
+                })
+                .then((response) => {
+
+                    author.value = '';
+                    message.value = '';
+                });
+        }
+    });
+
+    refreshBtn.addEventListener('click', function (e) {
+
+        fetch(url)
+            .then((response) => {
+
+                if (!response.ok) {
+
+                    console.error('Something get wrong!');
+
+                } else {
+
+                    return response.json();
+                }
+            })
+            .then((data) => {
+
+                messages.textContent = '';
+
+                for (let message of Object.keys(data)) {
+
+                    messages.textContent += `${data[message].author}: ${data[message].content}\n`;
+                }
+            });
+    });
 }
 
 attachEvents();
